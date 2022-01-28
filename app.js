e529b29f231e2b029a9642dfb79a137a2e899312
bundym <63752327+bundym@users.noreply.github.com>
2022-01-28 10:11:10 -0600

    2.4 Assignment
    Variables

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Hey Dev Ed') ;
+var health = 100; // this is the full health status
+
+let  x = 100;
+const y = 25;
+
+{  
+  let x = 50; // this is block variable
+}
+
+let z = x * y;
+
+const firstName = "Bundy"; // string variable
+
+const car = {type:"BMW", model:"M3", color:"white"}; // obejct variable using const
+
+// Add a property:
+car.owner = "Manriquez";
+
+// Display the property:
+
+
+
+
+
+
+
+
+console.log(health + x + z + " " + firstName );
+console.log("Car type and model is " + car.type + " " + car.model + " and owner is " + firstName + " " + car.owner);
