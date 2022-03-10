1579d644790758bffdaa72fe8188cb7151acbbb6
bundym <63752327+bundym@users.noreply.github.com>
2022-03-09 18:16:54 -0600

    Rock Paper Scissors

--- app.js
+++ app.js
@@ -1,54 +1,125 @@
-//STart of assignment 6.4
+//Module 7
 
-//Object and Object method
-const newPerson = {
-  firstName : "Bundy",
-  lastName  : "Manriquez",
-  age : 32,
-  eyeColor  : "brown",
-  height : "6ft 1in",
-  gender : "male",
-  weight : "185 lbs",
-  fullName : function() {
-    return this.firstName + " " + this.lastName;
-  },
-  measurements : function(){
-    return this.height + ", " + this.weight;
+const game = ()=> {
+  let pScore = 0;
+  let cScore = 0;
+
+  //Start the game
+  const startGame = ()=>{
+    const playBtn = document.querySelector('.intro button');
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+
+    playBtn.addEventListener('click', () =>{
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  //Play match
+  const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
+
+    hands.forEach(hand =>{
+      hand.addEventListener('animationend', function(){
+        this.style.animation ="";
+      })
+    })
+
+    //computer options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    options.forEach(option=>{
+        option.addEventListener('click', function() {
+
+          //computer choice
+          const computerNumber = Math.floor(Math.random() * 3);
+          const computerChoice = computerOptions[computerNumber];
+
+          //here is where we call compare hands
+          setTimeout(() => {
+            compareHands(this.textContent, computerChoice);
+
+          //Update images
+          playerHand.src =`./assets/${this.textContent}.png`;
+          computerHand.src =`./assets/${computerChoice}.png`;}, 2000)
+
+          //Animation
+          playerHand.style.animation = "shakePlayer 2s ease";
+          computerHand.style.animation = "shakeComputer 2s ease";
+
+        });
+    });    
+  };
+
+  const updateScore = () =>{
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
   }
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //update Text
+    const winner = document.querySelector('.winner');
+    //checking for a tie
+    if(playerChoice === computerChoice){
+      winner.textContent = 'It is a tie';
+      return;
+    }
+    //check for rock
+    if(playerChoice ==='rock'){
+      if(computerChoice === 'scissors'){
+        winner.textContent = 'Player Wins';
+        pScore++;
+        updateScore();
+        return;
+      }else{
+        winner.textContent = 'Computer Wins';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //check for paper
+    if(playerChoice ==='paper'){
+      if(computerChoice === 'scissors'){
+        winner.textContent = 'Computer Wins'
+        cScore++;
+        updateScore();
+        return;
+      }else{
+        winner.textContent = 'Player Wins';
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //check for scissors
+    if(playerChoice ==='scissors'){
+      if(computerChoice === 'rock'){
+        winner.textContent = 'Computer Wins'
+        cScore++;
+        updateScore();
+        return;
+      }else{
+        winner.textContent = 'Player Wins';
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  }
+
+   //Is call all the inner function
+ startGame();
+ playMatch();
 };
 
-const d = new Date(89, 10, 13);
+  //Start the game function
+  game();
 
-console.log(`I am ${newPerson.fullName()} and I am ${newPerson.age} years old.`+ 
-` My measurements are ${newPerson.measurements()}. I was born on ${d.toDateString()}.`);
-
-console.log(`My eyes are ${newPerson.eyeColor}.`);
-
-
-
-//Array and Array Method
-
-const characters = [
-  'Mario',
-  'Kirby',
-  'Link',
-  'Samus',
-  'Isabella'
-];
-
-const secondTier = [
-  'Incineroar',
-  'Pikachu',
-]
-
-characters.push('Captain Falcon');
-characters[characters.length] = 'Yoshi';
-console.log(characters.sort());
-
-
-document.getElementById("character").innerHTML = characters[2];
-
-const myCharacters = characters.concat(secondTier);
-
-console.log(`All characters I choose from are ${myCharacters}.`)
 
